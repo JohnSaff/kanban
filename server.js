f804618a7fb9fc822a7e7716056f3d1d2ad7fce9
JSafrany <j.c.safrany@gmail.com>
2020-10-15 11:06:48 +0100

    added createUser handlebars view and added server functionality to create users

--- server.js
+++ server.js
@@ -15,5 +15,22 @@ app.engine('handlebars', handlebars)
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
+async function sync() {
+ await sequelize.sync()
+}
+sync()
 app.listen(3000, () => console.log('web server running on port 3000'))
+
+//---------rendering create User page ------
+
+app.get('/users/create',(req,res)=>{
+    res.render('createUser')
+})
+
+//--------post request to add user to db from create user page
+
+app.post('/users/create',async (req,res)=>{
+    console.log('creating user')
+    const user = await User.create({username:req.body.username,avatar:req.body.avatar})
+    res.redirect(`/users/${user.id}`)
+})
